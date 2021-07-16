d373334e3dbb8c2a56c6456651ff38e8a078c651
binoy <binoy.18@cse.mrt.ac.lk>
2021-07-16 11:18:08 +0530

    refresh token endpoint using simplejwt

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='customer-register'),
-    path('login/', LoginView.as_view(), name='customer-login')
+    path('login/', LoginView.as_view(), name='customer-login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
